6370431ca0d00a808578b6ced754b224dadbfaa7
Filipe Ferreira <filipeffs10@gmail.com>
2018-05-29 17:55:23 -0300

    adding reactive form

--- src/pages/subscription/subscription.ts
+++ src/pages/subscription/subscription.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the SubscriptionPage page.
@@ -10,16 +11,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-subscription',
-  templateUrl: 'subscription.html',
+    selector: 'page-subscription',
+    templateUrl: 'subscription.html',
 })
 export class SubscriptionPage {
+    public subscriptionForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private formBuilder: FormBuilder
+    ) {
+        this.buildSubscriptionForm();
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SubscriptionPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SubscriptionPage');
+    }
 
+    buildSubscriptionForm() {
+        this.subscriptionForm = this.formBuilder.group({
+            name: [null, Validators.compose([
+                Validators.required
+            ])],
+            email: [null, Validators.compose([
+                Validators.required
+            ])],
+            birthday : [null, Validators.compose([
+                Validators.required
+            ])],
+            gender : [null, Validators.compose([
+                Validators.required
+            ])],
+            password : [null, Validators.compose([
+                Validators.required
+            ])],
+            passwordConfirmation : [null, Validators.compose([
+                Validators.required
+            ])],
+        });
+    }
+
+    public subscribe(): void {
+        if(!this.subscriptionForm.valid) {
+            console.log('formulário inválido...');
+        }
+    }
 }
