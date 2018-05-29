7cd77b7910615210c6841f254e34a15f3326845c
Filipe Ferreira <filipeffs10@gmail.com>
2018-05-29 12:02:43 -0300

    adding background and subscription page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { SubscriptionPage } from '../subscription/subscription'
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
+
+    public subscribe(): void {
+        console.log('go to subscription page ...');
+        this.navCtrl.push(SubscriptionPage);
+    }
 
 }
