e12f6b6ecf127de46886fa2605811d4cc0a0d9b3
Filipe Ferreira <filipeffs10@gmail.com>
2018-06-17 02:32:00 -0300

    preferencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { MainTabsPage } from '../pages/main-tabs/main-tabs';
     templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage: any = MainTabsPage;//HomePage;
+    rootPage: any = HomePage;
 
     constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
         platform.ready().then(() => {
